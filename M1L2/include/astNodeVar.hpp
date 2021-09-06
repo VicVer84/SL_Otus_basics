9abd265377ecbe3e61f3733ee37160dd2a8748c0
VicVer84 <vic_ver84@mail.ru>
2021-09-06 12:09:34 +0300

    removed ctor/dtor output for ASTNode

--- M1L2/include/astNodeVar.hpp
+++ M1L2/include/astNodeVar.hpp
@@ -6,11 +6,5 @@
 
 class Variable : public ASTNode {
   public:
-    Variable(const std::string &repr)
-        : ASTNode(repr) {
-			std::cout << "Var ctor: " << repr << '\n';
-		}
-	~Variable(){
-		std::cout << "Variable destructor. \n";
-	}
+    Variable(const std::string &repr) : ASTNode(repr) {}
 };
